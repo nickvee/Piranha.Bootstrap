2042e96d7ff2d333b1ae1a0a75bf22b491d11c82
Nick Vierros <nick@xqwzid.com.au>
2013-06-19 14:39:55 +1000

    Revert "Updated without Nuget Packages"
    This reverts commit 0be1dc3601027f7361120a26307afbdd8992f7e0.

---
+++ packages/PiranhaCMSMvc.2.0.3/content/Controllers/PageController.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Piranha.Mvc;
+
+namespace $rootnamespace$.Controllers
+{
+	/// <summary>
+	/// The page controller is the standard controller displaying a page
+	/// generated from the manager interface.
+	/// </summary>
+    public class PageController : SinglePageController
+    {
+		/// <summary>
+		/// Gets a standard page.
+		/// </summary>
+		/// <returns>The view result</returns>
+        public ActionResult Index() {
+			var model = GetModel() ;
+
+            return View(model.GetView(), model) ;
+        }
+    }
+}
---
+++ packages/PiranhaCMSMvc.2.0.3/content/Controllers/PostController.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Piranha.Mvc;
+
+namespace $rootnamespace$.Controllers
+{
+	/// <summary>
+	/// The post controller is the standard controller displaying a post
+	/// generated from the manager interface.
+	/// </summary>
+    public class PostController : SinglePostController
+    {
+		/// <summary>
+		/// Gets a standard post.
+		/// </summary>
+		/// <returns>The view result</returns>
+        public ActionResult Index() {
+			var model = GetModel() ;
+
+            return View(model.GetView(), model) ;
+        }
+    }
+}
